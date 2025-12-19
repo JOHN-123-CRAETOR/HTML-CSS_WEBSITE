1490de660119d6bf12da8dd2fed6be8bbc2c9f7b
John Boateng <boatwan419@gmail.com>
2025-12-19 21:58:55 +0000

    Add more features

--- index.js
+++ index.js
@@ -1,28 +1,67 @@
-const nameInput = document.querySelector('#name');
-const ageInput = document.querySelector('#age');
-const addBtn = document.getElementById("add-btn");
-const tableContainer = document.getElementById("table-container"); // a div to hold table
+const nameInput = document.querySelector("#name");
+const ageInput = document.querySelector("#age");
+const addBtn = document.querySelector("#add-btn");
+const nameMsg = document.getElementById("name-msg");
+const ageMsg = document.getElementById("age-msg");
+const gender = document.getElementById("gender");
 
-let idCounter = 0;
-let table;   
-let tbody;   
+
+let id = 0;
+let table;
+let tbody;
+
+
+addBtn.style.backgroundColor = "green";
 
 addBtn.addEventListener('click', () => {
+
     const name = nameInput.value.trim();
     const age = ageInput.value.trim();
+    const selectedGender = gender.value;
 
-    if (name === "" || age === "") {
-        alert("Please enter your name and age");
+    if (name === "" && age === ""){
+        nameMsg.textContent = "Enter a valid name";
+        ageMsg.textContent = "Enter a valid name";
+        ageInput.style.borderColor = "red";
+        nameInput.style.borderColor = "red";
+        return;
+    
+    
+    }else if(name === ""){
+        nameMsg.textContent = "Enter a valid name";
+        nameInput.style.borderColor = "red";
+        return;
+       
+    }
+    else if(age === ""){
+        ageMsg.textContent = "Enter a valid age";
+        ageInput.style.borderColor = "red";
         return;
     }
+    else{
+        ageMsg.textContent = "";
+        nameMsg.textContent = "";
+        ageInput.style.borderColor = "green";
+        nameInput.style.borderColor = "green";
+    }
 
-    
-    if (!table) {
-        table = document.createElement('table');
+    if(ageInput.value.length >= 6){
+       alert("Please enter a valid age");
+       ageInput.style.borderColor = "red";
+       nameInput.style.borderColor = "red";
+        return ;
+    }
+    else{
+        ageInput.style.borderColor = "green";
+        nameInput.style.borderColor = "green";
+    }
+
+
+    if(!table){
+        table = document.createElement("table");
         table.style.borderCollapse = "collapse";
-        table.style.margin = "20px auto";
 
-        const thead = document.createElement('thead');
+        const thead = document.createElement("thead");
         thead.innerHTML = `
             <tr>
                 <th>ID</th>
@@ -30,26 +69,30 @@ addBtn.addEventListener('click', () => {
                 <th>AGE</th>
                 <th>GENDER</th>
             </tr>
-        `;
+        `
+       tbody = document.createElement('tbody');
         table.appendChild(thead);
+        
+        document.body.appendChild(table);
 
-        tbody = document.createElement('tbody');
-        table.appendChild(tbody);
-
-        tableContainer.appendChild(table);
     }
 
-    
-    const tr = document.createElement('tr');
-    tr.innerHTML = `
-        <td>${++idCounter}</td>
-        <td>${name}</td>
-        <td>${age}</td>
-        <td>Male</td>
-    `;
+     const tr = document.createElement("tr");
 
-    tbody.appendChild(tr);
+        
+        tr.innerHTML = `
+        
+                <td>${++id}</td>
+                <td>${name}</td>
+                <td>${age}</td>
+                <td>${selectedGender}</td>
+            
 
-    nameInput.value = '';
-    ageInput.value = '';
-});
+        `
+
+        tbody.appendChild(tr);
+        table.appendChild(tbody);
+
+        nameInput.value = "";
+        ageInput.value = "";
+})
